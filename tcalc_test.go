62cf33d4aa8b85820a272b1553f517fe7dd294c9
Geoffrey White <108630660+geofpwhite@users.noreply.github.com>
2026-01-15 16:44:38 -0500

    adding functionality to copy on click (#7)
    * adding functionality to copy on click
    
    * clean up handleMouse function
    
    * moving test function to calc_test.go
    
    * and remove it from tcalc_test.go

--- tcalc_test.go
+++ tcalc_test.go
@@ -41,42 +41,6 @@ func TestBitPosition(t *testing.T) {
 	}
 }
 
-func TestExec(t *testing.T) {
-	testCases := []struct {
-		expression string
-		expected   int64
-		shouldFail bool
-	}{
-		{"1 + 3 + 2", 6, false},
-		{"1 * (3 + 2)*(5+6)", 55, false},
-		{"(2 * (3 + 2) - 1)+ 1 / 1", 10, false},
-		{"(2 * (3 + 2) - 1)+ 1 / 1*3", 30, false},
-		{"1 << 5", 32, false},
-		{"0+-1", 0, true},
-		{"1&2", 0, false},
-		{"1|2", 3, false},
-		{"1^2", 3, false},
-		{"1^3", 2, false},
-		{"~1", -2, false},
-		{"2>>1", 1, false},
-	}
-	for _, tc := range testCases {
-		s := calculator.NewState()
-		err := s.Exec(tc.expression)
-		if tc.shouldFail {
-			if err == nil {
-				t.Errorf("Expected failure for expression: %s", tc.expression)
-			}
-			continue
-		}
-		if err != nil {
-			t.Errorf("Unexpected error for expression %s: %v", tc.expression, err)
-		} else if s.Ans != tc.expected {
-			t.Errorf("For expression %s, expected %d but got %d", tc.expression, tc.expected, s.Ans)
-		}
-	}
-}
-
 func TestConfigHandleInput(t *testing.T) {
 	ap := ansipixels.NewAnsiPixels(30)
 	tests := []struct {
